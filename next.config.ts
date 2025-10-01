ac93196b0f26f2f1a30a7a95b7e6f28a8b6d538b
Raghuvansh <raghuvanshmani876@gmail.com>
2025-10-01 12:19:06 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1759314959280.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
